cc95751b349828a8130fdd83e151b5f28a151d78
nishan <bhattarainisham@gmail.com>
2025-11-22 10:00:15 +0545

    feat: add SignOutButton component and SafeScreen wrapper; implement color constants and utility functions
    - Implemented SignOutButton component for user sign-out functionality using Clerk.
    - Created SafeScreen component to handle safe area insets for better UI on different devices.
    - Added color constants for theming (coffee, forest, purple, ocean) in colors.js.
    - Introduced utility function formatDate for formatting date strings.
    - Developed styles for authentication, creation, and home screens to ensure consistent UI.

---
+++ mobile/app/_layout.jsx
@@ -0,0 +1,13 @@
+import { Slot, Stack } from "expo-router";
+import SafeScreen from "../components/safescreen.jsx";
+import { ClerkProvider, tokenCache } from "@clerk/clerk-expo";
+
+export default function RootLayout() {
+  return (
+   <ClerkProvider tokenCache={tokenCache}>
+     <SafeScreen>
+    <Slot  />
+    </SafeScreen>
+    </ClerkProvider>
+  );
+}
